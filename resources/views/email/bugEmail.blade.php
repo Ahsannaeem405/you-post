d2bc46fdebf65a3a6182b2ca5b18c03d0af40b2f
Ali Haider <73309177+alihdr000@users.noreply.github.com>
2023-11-22 18:13:56 +0500

    email template done

--- resources/views/email/bugEmail.blade.php
+++ resources/views/email/bugEmail.blade.php
@@ -1,30 +1,33 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>you-post</title>
 </head>
+
 <body>
-    <table width="100%">
+    <table border="0" cellspacing="0" cellpadding="0" align="center" style="width: 500px; margin: 0 auto;border-radius: 10px;background-color: #ddd;padding-left: 22px;padding-right: 22px;padding-bottom: 22px;">
         <tr>
             <td>
-                <h1>tes</h1>
+                <h1 style="font-size: 34px;font-family: system-ui;margin:0;">tes</h1>
             </td>
         </tr>
         <tr>
             <td>
-                <h2>xvdfg</h2>
+                <h2 style="font-size: 24px;font-family: system-ui;margin:0;">xvdfg</h2>
             </td>
         </tr>
         <tr>
             <td>
-                <p>gfdgd</p>
+                <p style="font-size: 18px;font-family: system-ui;margin-top: 10px;margin-bottom: 10px">gfdgd</p>
             </td>
         </tr>
         <tr>
             <td>
-                <p>Thank you</p>
+                <p style="font-size: 16px;font-family: system-ui;margin-top: 10px;margin-bottom: 10px">Thank you</p>
             </td>
         </tr>
     </table>
 </body>
+
 </html>
