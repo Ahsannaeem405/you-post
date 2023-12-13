80c7e1319b41c5676061dafef5db5b24c9ed2903
Muhammad-Saleem-Ali <116871330+Muhammad-Saleem-Ali@users.noreply.github.com>
2023-12-13 18:22:43 +0500

    loader

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -18,26 +18,39 @@
     height: 50px;
     animation: spin 1s linear infinite;
     display: none; /* Initially hidden */
+    position: absolute;
+    top: -52px;
+    left: -28px;
+    z-index: 9;
 }
 
 @keyframes spin {
     0% { transform: rotate(0deg); }
     100% { transform: rotate(360deg); }
 }
+
+.otp-container {
+    height: 90vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+
 </style>
 @if($errors->has('otp'))
     <script>
         toastr.error("Invalid or expired OTP.", "Error");
     </script>
 @endif
-<div class="container">
+<div class="container otp-container">
     <div class="row justify-content-center">
         <div class="col-md-8">
         <form method="POST" action="{{ route('verification.verify') }}">
                         @csrf
                         <div class="form-group row">
                             <div class="col-md-12">
-                            <div id="loader" class="loader"></div>
+
 
                                 <!-- <input id="verification_code" type="text" class="form-control @error('verification_code') is-invalid @enderror" name="verification_code" required> -->
                                 <!-- <p id="otp-timer">Remaining Time: <span id="minutes"></span> minutes <span id="seconds"></span> seconds</p> -->
@@ -46,21 +59,25 @@
                     <h6>Please enter the otp <br> to verify your account</h6>
                     <div> <span>A otp has been sent to</span> <small>email</small> </div>
                     <div id="otp" class="inputs d-flex flex-row justify-content-center mt-2">
-                        <input required class="m-2 text-center form-control rounded" type="number" id="first" name ="first" maxlength="1" /> 
-                        <input required class="m-2 text-center form-control rounded" type="number" id="second" name ="second" maxlength="1" /> 
-                        <input required class="m-2 text-center form-control rounded" type="number" id="third" name ="third" maxlength="1" /> 
-                        <input required class="m-2 text-center form-control rounded" type="number" id="fourth" name ="fourth" maxlength="1" /> 
-                        
-                       </div> <div class="mt-4"> 
+                        <input required class="m-2 text-center form-control rounded" type="number" id="first" name ="first" maxlength="1" />
+                        <input required class="m-2 text-center form-control rounded" type="number" id="second" name ="second" maxlength="1" />
+                        <input required class="m-2 text-center form-control rounded" type="number" id="third" name ="third" maxlength="1" />
+                        <input required class="m-2 text-center form-control rounded" type="number" id="fourth" name ="fourth" maxlength="1" />
+
+                       </div>
+                        <div class="loader-p position-relative">
+                        <div id="loader" class="loader"></div>
+                        </div>
+                       <div class="mt-4">
                        <button type="submit" class="btn  px-4 validate" id="validateButton" @if($remainingTime_expire) disabled @endif>Verify</button>
                        </div>
-                     </div> 
+                     </div>
                      <div class="card-2">
                       <div class="content d-flex justify-content-center align-items-center">
                          <span>Didn't get the code</span>
-                          <a href="#" class="text-decoration-none ms-3" id="resendButton">Resend(<span id = "seconds"></span>)</a> 
+                          <a href="#" class="text-decoration-none ms-3" id="resendButton">Resend(<span id = "seconds"></span>)</a>
                         </div>
-                     </div> 
+                     </div>
                     </div>
                  </div>
                    @error('verification_code')
@@ -71,13 +88,13 @@
                             </div>
                         </div>
 
-                       
+
                     </form>
     </div>
 </div>
 
 <script>
-document.addEventListener("DOMContentLoaded", function(event) {  
+document.addEventListener("DOMContentLoaded", function(event) {
 function OTPInput() {
             const inputs = document.querySelectorAll('#otp > *[id]');
             for (let i = 0; i < inputs.length; i++) { inputs[i].addEventListener('keydown', function(event) { if (event.key==="Backspace" ) { inputs[i].value='' ; if (i !==0) inputs[i - 1].focus(); } else { if (i===inputs.length - 1 && inputs[i].value !=='' ) { return true; } else if (event.keyCode> 47 && event.keyCode < 58) { inputs[i].value=event.key; if (i !==inputs.length - 1) inputs[i + 1].focus(); event.preventDefault(); } else if (event.keyCode> 64 && event.keyCode < 91) { inputs[i].value=String.fromCharCode(event.keyCode); if (i !==inputs.length - 1) inputs[i + 1].focus(); event.preventDefault(); } } }); } } OTPInput();
@@ -116,38 +133,38 @@ function OTPInput() {
 
 <script>
     // Handle click event for the edit link
-    $('#resendButton').click(function(e) {  
-          
-              e.preventDefault();    
+    $('#resendButton').click(function(e) {
+
+              e.preventDefault();
               var csrfToken = $('meta[name="csrf-token"]').attr('content');
               var loader = $('#loader');  // Assuming you have an element with id 'loader'
 
 
         $.ajax({
-            url: '/send-otp', 
+            url: '/send-otp',
             method: 'POST',
-            data: {_token: csrfToken}, 
+            data: {_token: csrfToken},
             beforeSend: function() {
             // Show loader before sending the request
             loader.show();
             validateButton.disabled = true;
-            resendButton.style.pointerEvents = 'none'; 
+            resendButton.style.pointerEvents = 'none';
         },
 
-            
+
             success: function(data) {
-                // $('#resendButton').hide();            
+                // $('#resendButton').hide();
                 // $('#verifyButton').show();
                  remainingTime = data.remainingTime;
-                 remainingTime_expire = data.remainingTime_expire;  
+                 remainingTime_expire = data.remainingTime_expire;
                  if (!remainingTime_expire) {
                     validateButton.disabled = false;
                 } else {
-                    
-                    validateButton.disabled = true;
-                }            
 
-                 updateTimer(); 
+                    validateButton.disabled = true;
+                }
+
+                 updateTimer();
                  loader.hide();
 
 
